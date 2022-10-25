a1b58d3f0c554e5013f950d6e172f705a4d1b308
sohaib88 <roomi9966@gmail.com>
2022-10-25 21:15:18 +0500

    Deleted unwanted code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import AddToDo from './AddToDo';
 import CheckB from './Counter';
 // import './App.css'
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { StoreProvider } from "./TodoStore";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StoreProvider>
+  <React.StrictMode>
     <App />
-  </StoreProvider>
+  </React.StrictMode>
 );
